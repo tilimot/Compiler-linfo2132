65b8efa1082a8804186ac1c9a553e363f9dbed97
Sacha <sacha.dementen@student.uclouvain.be>
2025-04-14 21:36:14 +0200

    tests semantic type et operator

--- test/TestSemantic.java
+++ test/TestSemantic.java
@@ -1,5 +1,8 @@
 import compiler.Exception.MissingConditionException;
+import compiler.Exception.OperatorException;
+import compiler.Exception.TypeException;
 import compiler.Lexer.Lexer;
+import compiler.Parser.Grammar.Ast;
 import compiler.Parser.Parser;
 import compiler.Parser.Semantic;
 import org.junit.Test;
@@ -16,6 +19,8 @@ public class TestSemantic {
         Lexer lexer = new Lexer(reader);
         return new Parser(lexer);
     }
+
+
 
     @Test
     public void ifLoops_WithoutCondition_Should_Throws_MissingConditionError() throws Exception {
@@ -71,7 +76,8 @@ public class TestSemantic {
 
     @Test
     public void forLoops_WithMissingCondition2_Should_Throws_MissingConditionError() throws Exception {
-        String input = "for(i,,110,1){}";
+        String input = "fun square(v int) int { \n" +
+                "\n" +" for(i,,110,1){} \n" + "}";
         Parser parser = getParser(input);
         try {
             parser.getAST();
@@ -134,18 +140,42 @@ public class TestSemantic {
     }
 
     @Test
-    public void badAssignmentType() throws Exception {
-        String input = "a string = 1;";
+    public void semanticAnalysis_Should_Pass_On_IntPlusInt() throws Exception {
+        String input = "x int = 3 + 4;";
         Parser parser = getParser(input);
-        Semantic semantic = new Semantic(parser.getAST());
-        try {
-            parser.getAST();
-            fail("MissingConditionError wasn't raise");
-        } catch (MissingConditionException e) {
-            // Assert
-            assertEquals("MissingConditionError: Must attribute a condition", e.getMessage());
-        }
+        parser.getAST(); // ne doit pas lever d’exception
     }
+
+    @Test(expected = OperatorException.class)
+    public void semanticAnalysis_Should_Throw_OperatorError_On_IntPlusString() throws Exception {
+        String input = "x int  = 3 + \"hello\";";
+        Parser parser = getParser(input);
+        Ast ast = parser.getAST();
+        ast.semanticAnalysis();
+    }
+
+
+
+    @Test(expected = OperatorException.class)
+    public void semanticAnalysis_Should_Throw_OperatorError_On_MixedTypesWithIdentifier() throws Exception {
+        String input = " x int= 5 + \"abc\" + y;";
+        Parser parser = getParser(input);
+        Ast ast = parser.getAST();
+        ast.semanticAnalysis(); // Doit lever OperatorException
+    }
+
+
+    @Test(expected = TypeException.class)
+    public void semanticAnalysis_Should_Throw_TypeError_On_AssignmentMismatch() throws Exception {
+        String input = " x int = \"hello\";";
+        Parser parser = getParser(input);
+        Ast ast = parser.getAST();
+        ast.semanticAnalysis(); // Doit lever TypeException
+    }
+
+
+
+
 
 
 
