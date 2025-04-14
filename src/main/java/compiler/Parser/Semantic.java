ea504a95564519f418346c2d6705613952b7ca9c
Sacha <sacha.dementen@student.uclouvain.be>
2025-04-14 14:51:45 +0200

    suppression de matcher et method, ajout de semantic

---
+++ src/main/java/compiler/Parser/Semantic.java
@@ -0,0 +1,17 @@
+package compiler.Parser;
+
+import compiler.Parser.Grammar.Ast;
+
+public class Semantic {
+
+    public Ast ast;
+
+    public Semantic(Ast ast ){
+        this.ast = ast;
+    }
+
+    public static boolean startAnalysis () throws Exception{
+
+    }
+
+}
