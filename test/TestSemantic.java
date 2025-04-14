a0aad8bf0429c5a1c861afdc278bf0fba356120e
tilimot <timothee.butenda@gmail.com>
2025-04-14 18:43:18 +0200

    Throw MissingException

--- test/TestSemantic.java
+++ test/TestSemantic.java
@@ -1,33 +1,142 @@
+import compiler.Exception.MissingConditionException;
 import compiler.Lexer.Lexer;
 import compiler.Parser.Grammar.*;
 import compiler.Parser.Parser;
 import org.junit.Test;
+import org.junit.runners.Parameterized;
 
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
 public class TestSemantic {
-    public Parser getParser(String input){
+
+    public Parser getParser(String input) {
         Reader reader = new StringReader(input);
         Lexer lexer = new Lexer(reader);
         return new Parser(lexer);
     }
 
     @Test
-    public void testBasicInput() throws Exception {
-        String input = "a+2*'hello'/true";
-        System.out.println(input);
-        Reader reader = new StringReader(input);
-
-        Lexer lexer = new Lexer(reader);
-        System.out.println(lexer);
-        Parser parser = new Parser(lexer);
-
-        ArrayList<Expression> expressions = parser.parseExpressions();
-        assertNotNull(expressions);
+    public void ifLoops_WithoutCondition_Should_Throws_MissingConditionError() throws Exception {
+        String input = "if(){}";
+        Parser parser = getParser(input);
+        try {
+            parser.getAST();
+            fail("MissingConditionError wasn't raise");
+        } catch (MissingConditionException e) {
+            // Assert
+            assertEquals("MissingConditionError: Must attribute a condition", e.getMessage());
+        }
     }
+
+    @Test
+    public void forLoops_WithoutCondition_Should_Throws_MissingConditionError() throws Exception {
+        String input = "for(){}";
+        Parser parser = getParser(input);
+        try {
+            parser.getAST();
+            fail("MissingConditionError wasn't raise");
+        } catch (MissingConditionException e) {
+            // Assert
+            assertEquals("MissingConditionError: Must attribute a condition", e.getMessage());
+        }
+    }
+
+    @Test
+    public void whileLoops_WithoutCondition_Should_Throws_MissingConditionError() throws Exception {
+        String input = "while(){}";
+        Parser parser = getParser(input);
+        try {
+            parser.getAST();
+            fail("MissingConditionError wasn't raise");
+        } catch (MissingConditionException e) {
+            // Assert
+            assertEquals("MissingConditionError: Must attribute a condition", e.getMessage());
+        }
+    }
+
+    @Test
+    public void forLoops_WithMissingCondition_Should_Throws_MissingConditionError() throws Exception {
+        String input = "for(,1,110,1){}";
+        Parser parser = getParser(input);
+        try {
+            parser.getAST();
+            fail("MissingConditionError wasn't raise");
+        } catch (MissingConditionException e) {
+            // Assert
+            assertEquals("MissingConditionError: Must attribute a condition", e.getMessage());
+        }
+    }
+
+    @Test
+    public void forLoops_WithMissingCondition2_Should_Throws_MissingConditionError() throws Exception {
+        String input = "for(i,,110,1){}";
+        Parser parser = getParser(input);
+        try {
+            parser.getAST();
+            fail("MissingConditionError wasn't raise");
+        } catch (MissingConditionException e) {
+            // Assert
+            assertEquals("MissingConditionError: Must attribute a condition", e.getMessage());
+        }
+    }
+    @Test
+    public void forLoops_WithMissingCondition3_Should_Throws_MissingConditionError() throws Exception {
+        String input = "for(i,1,,1){}";
+        Parser parser = getParser(input);
+        try {
+            parser.getAST();
+            fail("MissingConditionError wasn't raise");
+        } catch (MissingConditionException e) {
+            // Assert
+            assertEquals("MissingConditionError: Must attribute a condition", e.getMessage());
+        }
+    }
+
+    @Test
+    public void forLoops_WithMissingCondition4_Should_Throws_MissingConditionError() throws Exception {
+        String input = "for(i,1,110,){}";
+        Parser parser = getParser(input);
+        try {
+            parser.getAST();
+            fail("MissingConditionError wasn't raise");
+        } catch (MissingConditionException e) {
+            // Assert
+            assertEquals("MissingConditionError: Must attribute a condition", e.getMessage());
+        }
+    }
+
+    @Test
+    public void forLoops_WithMissingCondition5_Should_Throws_MissingConditionError() throws Exception {
+        String input = "for(i,1){}";
+        Parser parser = getParser(input);
+        try {
+            parser.getAST();
+            fail("MissingConditionError wasn't raise");
+        } catch (MissingConditionException e) {
+            // Assert
+            assertEquals("MissingConditionError: Must attribute a condition", e.getMessage());
+        }
+    }
+
+    @Test
+    public void forLoops_WithMissingCondition6_Should_Throws_MissingConditionError() throws Exception {
+        String input = "for(i,1,){}";
+        Parser parser = getParser(input);
+        try {
+            parser.getAST();
+            fail("MissingConditionError wasn't raise");
+        } catch (MissingConditionException e) {
+            // Assert
+            assertEquals("MissingConditionError: Must attribute a condition", e.getMessage());
+        }
+    }
+
+
 
 }
