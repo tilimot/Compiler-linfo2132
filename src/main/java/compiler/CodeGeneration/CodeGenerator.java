f5677a09b1763cc2ccad5bb723c74f1b06655d13
tilimot <timothee.butenda@gmail.com>
2025-05-18 18:08:20 +0200

    generateGlobalVariable for simple variable assignement + generateAST has been integrated into generateFileClasse + generateMain is not currently usable anymore

--- src/main/java/compiler/CodeGeneration/CodeGenerator.java
+++ src/main/java/compiler/CodeGeneration/CodeGenerator.java
@@ -31,98 +31,127 @@ public class CodeGenerator{
         this.indexTable = new IndexTable(null,0);
     }
 
+
     public void generateFileClass() throws Exception{
+
         // CreateClass
-        cw.visit(Opcodes.V1_8, ACC_PUBLIC, this.generatedClass, null, "java/lang/Object", null);
-        generateMainMethod(); //TODO: should generate AST and generateAST should generateMAIN
-    }
-
-    public void generateMainMethod() throws Exception {
-        MethodVisitor mv = cw.visitMethod(ACC_PUBLIC | ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
-        mv.visitCode();
-
-        // Should be the inverse. GenerateAST should call generateMainMethod
-        generateAST(mv,this.ast, this.indexTable);
+        this.cw.visit(Opcodes.V1_8, ACC_PUBLIC, this.generatedClass, null, "java/lang/Object", null);
 
 
-        // Fin de la méthode
-        mv.visitInsn(RETURN);
+        // Ast grammar: AST -> Constants Records GlobalVariables Functions
+        Ast ast = this.ast;
 
-        //mv.visitMaxs(100, indexTable.getCurrent_index());
-        mv.visitMaxs(0, 0);
-        mv.visitEnd();
 
+        // Generate elements of AST
+        ArrayList<Constant> cst = ast.getConstant();
+        ArrayList<Record> records = ast.getRecords();
+
+        // generate Global Variable only if this is not empty
+        ArrayList<Statement> globalVariables = ast.getGlobalVariables();
+        if (!globalVariables.isEmpty()) {
+            // Static Bloc : static {x = 10;}
+           generateMoreGlobalVariable(this.cw, globalVariables);
+        }
+
+        ArrayList<FunctionStatement> functions = ast.getFunctions();
+
+
+        //generateMainMethod(); //TODO: should generate AST and generateAST should generateMAIN
+
+
+        cw.visitEnd();
+        // generateFile into Bytecode
         byte[] bytecode = cw.toByteArray();
         java.nio.file.Files.write(java.nio.file.Paths.get(this.generatedClass+".class"), bytecode);
     }
 
-    public void  generateAST(MethodVisitor mv, Ast ast, IndexTable indexTable) throws Exception {
-        // TODO: INCOMPLETE -> Manage only restraint globalVariable
 
-        // Ast grammar: AST -> Constants Records GlobalVariables Functions
-        ArrayList<Constant> cst = ast.getConstant();
-        ArrayList<Record> records = ast.getRecords();
-
-
-        // generate Global Variable
-        ArrayList<Statement> globalVariables = ast.getGlobalVariables();
-        if (globalVariables.size()>0) {
-            generateGlobalVariable(mv, globalVariables, indexTable);
+    public String getTypeDescriptor(TokenType type) throws Exception {
+        /**
+         * @param type: A TokenType  .
+         * @return Return the corresponding TypeDescriptor (used by ASM)
+         */
+        if (type.equals(TokenType.INTEGER)){
+            return "I";
         }
-
-
-
-        ArrayList<FunctionStatement> functions = ast.getFunctions();
-
-        generateMainMethod();
-
-
+        else if(type.equals(TokenType.FLOAT)){
+            return "F";
+        }
+        else if(type.equals(TokenType.STRINGS)){
+            return "Ljava/lang/String;";
+        }
+        else if(type.equals(TokenType.BOOLEAN)){
+            //Booleans
+            return "Z";
+        }
+        else{
+            throw new Exception("Code generation error. Non supported type provided: "+ type);
+        }
     }
 
-    public void generateGlobalVariable(MethodVisitor mv, ArrayList<Statement> globalVariables, IndexTable indexTable) throws Exception {
+
+    public void generateMoreGlobalVariable(ClassWriter cw, ArrayList<Statement> globalVariables ) throws Exception {
+        // Static Bloc : static {x = 10; ...}
+        MethodVisitor clinit = cw.visitMethod(ACC_STATIC, "<clinit>", "()V", null, null);
+        clinit.visitCode();
+
+        // Adding fields:  public static x=10
+        for(Statement globVar: globalVariables) {
+            generateGlobalAssignmentVariable(cw, clinit,  globVar);
+        }
+        clinit.visitInsn(RETURN);
+        clinit.visitMaxs(0, 0);
+        clinit.visitEnd();
+    }
+
+
+    public void generateGlobalAssignmentVariable(ClassWriter cw, MethodVisitor clinit,Statement globalVariable) throws Exception {
         //TODO: Must call different generate Assignement depending if this is a simple variable assignment (i.e: a int= 1+2), array attribution (i.e: c int[]= array [5]), or else
-        //Currently consider only simple variable assignement (i.e: a int= 1+2)
+        //Currently consider only simple variable assignement (i.e: a int= 1+2). Miss array assignement, records attribute assignement, declaration
 
-        for(Statement stmt: globalVariables){
-            AssignementStatement assign = (AssignementStatement) stmt;
-            generateVariableAssignment(mv,assign, indexTable);
-        }
+        AssignementStatement assignment = (AssignementStatement) globalVariable;
 
-    }
-
-    public void generateFunction(MethodVisitor mv, ArrayList<FunctionStatement> functions, IndexTable indexTable){
-        int currentVarIndex = indexTable.getCurrent_index();
-        for(FunctionStatement function: functions) {
-            String methodName = function.getIdentifier();
-            ArrayList<Type> returnType = function.getReturn_type();
-            ArrayList<FuncParam> params = function.getParams();
-            Block block = function.getBlock();
-        }
-    }
-
-    public void generateVariableAssignment(MethodVisitor mv,AssignementStatement assignment, IndexTable indexTable) throws Exception {
-        //TODO: INCOMPLETE --> manage only rightSide. Must store identifier id in IndexTable
-        // Manage assignement
-
-        // RightSide - store identifier in IndexTable
+        // LeftSide - store identifier in IndexTable
         LeftSide ls = assignment.leftSide;
+
         String identifier = ls.getIdentifier();
-        indexTable.addIdentifier(identifier);
 
-        // LeftSide - generate expression
+        TokenType tokenType = ls.getType().getType(); // 1st get type to have the Type object, the 2nd is to have the TokenType from the type
+        String td = this.getTypeDescriptor(tokenType);
+
+
+        // RightSide - generate expression
         RightSideExpressions rs = (RightSideExpressions) assignment.rightSide;
-        generateExpression(mv,rs.expressions);
+        ArrayList<Expression> expressions = rs.getExpressions();
 
-        // Store the result
-        int varindex = indexTable.getCurrent_index();
-        mv.visitVarInsn(ISTORE, varindex);
+
+        // Add field: static type identifier;
+        cw.visitField( ACC_PUBLIC + ACC_STATIC, identifier, td, null, null).visitEnd();
+
+        // Generate the expression
+        generateExpression(clinit, expressions,"gv", td);
+
+        // Initialize the field
+        clinit.visitFieldInsn(Opcodes.PUTSTATIC, this.generatedClass, identifier, td);
+
     }
 
-    public void generateExpression(MethodVisitor mv, ArrayList<Expression> expressions) throws Exception {
-        /*
-        * Add on stack each element of the expression.
-        * When term =2, apply the given operation between the 2 elements on stack
-        */
+
+    public void generateExpression(MethodVisitor mv, ArrayList<Expression> expressions, String statementType, String typeDescriptor) throws Exception {
+        /**
+         * Add on stack each element of the expression.
+         * When term =2, apply the given operation between the 2 elements on stack
+         *
+         * @param statementType  Allow to know if the value of a variable is stored on the IndexTable or in the field table class.
+         *                     "gv": GlobalVariable -> value stored on field table
+         *                     "reg": Regular       -> value stored in indexTable
+         * @param identifier  Identifier of the variable. Used only to retrieve in value of variable in case of statementType = "gv".
+         *                    If no "gv", can be set to "".
+         * @param typeDescriptor typeD of the variable. Used only to retrieve in value of variable in case of statementType = "gv".
+         *                       If no "gv", can be set to "".
+         */
+
+
         // Currently only manage Sum of int
         Boolean addition = false;
         Boolean substraction = false;
@@ -172,9 +201,18 @@ public class CodeGenerator{
                 term +=1;
             }
             else if(tp.equals(TokenType.IDENTIFIER)){
-                int varIndex = indexTable.getIndexIdentifier(val);
-                mv.visitVarInsn(ILOAD, varIndex);
-                term+=1;
+                if (statementType.equals("reg")) {
+                    int varIndex = indexTable.getIndexIdentifier(val);
+                    mv.visitVarInsn(ILOAD, varIndex);
+                    term += 1;
+                }
+                else if(statementType.equals("gv")){
+                    mv.visitFieldInsn(GETSTATIC, this.generatedClass, val, typeDescriptor);
+                    term+=1;
+                }
+                else{
+                    throw new Exception("Issue in CodeGeneration. WrongS StatementType parameters in generateExpression Method");
+                }
             }
 
             if(term==2){ // if there is 2 terms, it means there must be an operation
@@ -202,4 +240,83 @@ public class CodeGenerator{
         }
 
     }
+
+
+    public void generateFunction(MethodVisitor mv, ArrayList<FunctionStatement> functions, IndexTable indexTable){
+
+
+        int currentVarIndex = indexTable.getCurrent_index();
+        for(FunctionStatement function: functions) {
+            String methodName = function.getIdentifier();
+            ArrayList<Type> returnType = function.getReturn_type();
+            ArrayList<FuncParam> params = function.getParams();
+            Block block = function.getBlock();
+        }
+    }
+
+    /*
+
+    public void generateMainMethod() throws Exception {
+        MethodVisitor mv = cw.visitMethod(ACC_PUBLIC | ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+        mv.visitCode();
+
+        // Should be the inverse. GenerateAST should call generateMainMethod
+        generateAST(mv,this.ast, this.indexTable);
+
+
+        // Fin de la méthode
+        mv.visitInsn(RETURN);
+
+        //mv.visitMaxs(100, indexTable.getCurrent_index());
+        mv.visitMaxs(0, 0);
+        mv.visitEnd();
+
+        byte[] bytecode = cw.toByteArray();
+        java.nio.file.Files.write(java.nio.file.Paths.get(this.generatedClass+".class"), bytecode);
+    }
+
+
+    public void  generateAST(MethodVisitor mv, Ast ast, IndexTable indexTable) throws Exception {
+        // TODO: INCOMPLETE -> Manage only restraint globalVariable
+
+        // Ast grammar: AST -> Constants Records GlobalVariables Functions
+        ArrayList<Constant> cst = ast.getConstant();
+        ArrayList<Record> records = ast.getRecords();
+
+
+        // generate Global Variable
+        ArrayList<Statement> globalVariables = ast.getGlobalVariables();
+        if (globalVariables.size()>0) {
+            generateGlobalVariable(mv, globalVariables, indexTable);
+        }
+
+
+
+        ArrayList<FunctionStatement> functions = ast.getFunctions();
+
+        generateMainMethod();
+
+
+    }*/
+
+
+    public void generateVariableAssignment(MethodVisitor mv,AssignementStatement assignment, IndexTable indexTable) throws Exception {
+        //TODO: INCOMPLETE --> manage only rightSide. Must store identifier id in IndexTable
+        // Manage assignement
+
+        // RightSide - store identifier in IndexTable
+        LeftSide ls = assignment.leftSide;
+        String identifier = ls.getIdentifier();
+        indexTable.addIdentifier(identifier);
+
+        // LeftSide - generate expression
+        RightSideExpressions rs = (RightSideExpressions) assignment.rightSide;
+        generateExpression(mv,rs.expressions,"gv", "");
+
+        // Store the result
+        int varindex = indexTable.getCurrent_index();
+        mv.visitVarInsn(ISTORE, varindex);
+    }
+
+
 }
--- src/main/java/compiler/Parser/Grammar/RightSideExpressions.java
+++ src/main/java/compiler/Parser/Grammar/RightSideExpressions.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 public class RightSideExpressions extends RightSide {
     public ArrayList<Expression> expressions;
+
+    public  ArrayList<Expression> getExpressions(){
+        return this.expressions;
+    }
+
     public RightSideExpressions(ArrayList<Expression> expressions){
         this.expressions= expressions;
     }
